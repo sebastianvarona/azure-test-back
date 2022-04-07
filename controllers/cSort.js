1e48accec1c6b44cb96260f2e9dbe6d6dc00fca4
Sebastian Varona <sebastianvarona@gmail.com>
2022-04-07 13:50:26 -0500

    sorteo route working

--- controllers/cSort.js
+++ controllers/cSort.js
@@ -17,51 +17,40 @@ function create(req, res) {
 
 }
 
-function show(req, res) {
-    if (req.body.error) return res.status(500).send({ error });
-    if (!req.body.sorteos) return res.status(404).send({ message: 'Not Found' });
-    let sorteos = req.body.sorteos;
-    return res.status(200).send({ sorteos });
+async function show(req, res) {
+    try {
+        const user = await Sorteo.findById(req.params.id);
+        res.status(200).json(user);
+    } catch (err) {
+        res.status(500).json(err);
+    }
 }
 
-function update(req, res) {
-    if (req.body.error) return res.status(500).send({ error });
-    if (!req.body.sorteos) return res.status(404).send({ message: 'Not Found' });
-    let sorteo = req.body.sorteos[0];
-    sorteo = Object.assign(sorteo, req.body);
-    sorteo.save()
-        .then(sorteo => res.status(200).send({ message: 'Sort Updated', sorteo })
-        ).catch(err => res.status(500).send({ err }))
+async function update(req, res) {
+    try {
+        const sorteo = await Sorteo.findByIdAndUpdate(req.params.id, {
+            $set: req.body,
+        });
+        res.status(200).send({ message: 'User Updated', sorteo });
+    } catch (err) {
+        res.status(500).send(err);
+    }
 }
 
-function deleted(req, res) {
-    if (req.body.error) return res.status(500).send({ error });
-    if (!req.body.sorteos) return res.status(404).send({ message: 'Not Found' });
-    req.body.sorteos[0].remove()
-        .then(sorteo => {
-            res.status(200).send({ message: 'Sort removed', sorteo })
-        }
-        ).catch(err => res.status(500).send({ err }));
+async function deleted(req, res) {
+    try {
+        const sorteo = await Sorteo.findByIdAndDelete(req.params.id);
+        res.status(200).send({ message: 'User Deleted', sorteo });
+    } catch (err) {
+        res.status(500).send(err);
+    }
 }
 
-function find(req, res, next) {
-    let query = {};
-    query[req.params.key] = req.params.value
-    Sorteo.find(query).then(sorteos => {
-        if (!sorteos.length) return next();
-        req.body.sorteos = sorteos;
-        return next();
-    }).catch(err => {
-        req.body.error = err;
-        next();
-    })
-}
 
 module.exports = {
     listall,
     show,
     create,
     update,
-    deleted,
-    find,
+    deleted
 }
