07e73bc3acbee4134bccbcd01767944bf64151fa
Alvaro Jose Santos <69470882+bipopa@users.noreply.github.com>
2022-04-07 12:02:34 -0500

    Update router.js

--- routes/router.js
+++ routes/router.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cViewer = require('../controllers/cViewer');
 const cStreamer = require('../controllers/cStreamer');
+const cSort= require('../controllers/cSort');
 
 const router = express.Router();
 
@@ -12,10 +13,15 @@ router.put('/viewer/:key/:value', cViewer.find, cViewer.update)
 router.delete('/viewer/:key/:value', cViewer.find, cViewer.deleted)
 
 // Streamer
-router.get('/streamer/', cViewer.listall);
-router.post('/streamer/', cViewer.create)
-router.get('/streamer/:key/:value', cViewer.find, cViewer.show)
-router.put('/streamer/:key/:value', cViewer.find, cViewer.update)
-router.delete('/streamer/:key/:value', cViewer.find, cViewer.deleted)
-
+router.get('/streamer/', cStremer.listall);
+router.post('/streamer/', cStreamer.create)
+router.get('/streamer/:key/:value', cStreamer.find, cStreamer.show)
+router.put('/streamer/:key/:value', cStreamer.find, cStreamer.update)
+router.delete('/streamer/:key/:value', cStreamer.find, cStreamer.deleted)
+//Sorteo
+router.get('/sorteo/', cSort.listall);
+router.post('/sorteo/', cSort.create)
+router.get('/sorteo/:key/:value', cSort.find, cSort.show)
+router.put('/sorteo/:key/:value', cSort.find, cSort.update)
+router.delete('/sorteo/:key/:value', cSort.find, cSort.deleted)
 module.exports = router;
